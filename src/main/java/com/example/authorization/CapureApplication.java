24420ab06efd219e2e3336b23c4304c758d6a3e2
Wanderlei Souza <wandi.darko@gmail.com>
2024-04-28 10:53:21 -0300

    first code commit

---
+++ src/main/java/com/example/authorization/CapureApplication.java
@@ -0,0 +1,13 @@
+package com.example.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authorization/CapureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
